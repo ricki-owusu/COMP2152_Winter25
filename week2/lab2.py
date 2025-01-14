e83e05668fb888add44b76f4aca3496b1822c12e
Ricki Owusu-Ansah <ricki.owusu-ansah@georgebrown.ca>
2025-01-14 10:29:19 -0500

    Py function example

--- week2/lab2.py
+++ week2/lab2.py
@@ -4,3 +4,7 @@ print("Elements: ", elements)
 
 #def funct_name():
 #   return True
+#def say_greeting(name, message="hi"):
+#    print(f"{message}, {name}")
+#say_greeting("Ricki")
+#say_greeting("Ricki", "Hello")
