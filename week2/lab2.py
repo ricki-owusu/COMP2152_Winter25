232e86ce70c103e0ae06981d8acdf045e5ba8e56
Ricki Owusu-Ansah <ricki.owusu-ansah@georgebrown.ca>
2025-01-14 10:22:53 -0500

    add elements array

---
+++ week2/lab2.py
@@ -0,0 +1,6 @@
+elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
+print("Elements: ", elements)
+# git add . && git commit -m "add elements array" && git push
+
+#def funct_name():
+#   return True
