6be094ac1c009942c464684dcc8352b3c2029e71
Ricki Owusu-Ansah <ricki.owusu-ansah@georgebrown.ca>
2025-01-14 10:55:00 -0500

    use randint and if elif and else condition makers

--- week2/lab2.py
+++ week2/lab2.py
@@ -1,3 +1,5 @@
+import random
+
 elements = ["Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon"]
 print("Elements: ", elements)
 # git add . && git commit -m "add elements array" && git push
@@ -16,3 +18,18 @@ def get_valid_int_input(prompt):
         except ValueError:
             print("Error: Please enter a valid integer")
             continue
+try:
+    elements_selected = get_valid_int_input("Enter the index of the element you would like to select: ")
+    # Roll dice
+    elementRoll = random.randint(1,6)
+    totalNum = elements_selected + elementRoll
+
+    # Print the result based on the totalNum
+    if elementRoll <= 2:
+        print("You rolled a weak element, fren.")
+    elif elementRoll <= 4:
+        print("You rolled a mid element, fren.")
+    else:
+        print("You rolled a strong element, fren.")
+except Exception as e:
+    print("")
