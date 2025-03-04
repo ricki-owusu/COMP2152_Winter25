ff71907aa19398075e4b19959af62756b5abcda9
Ricki Owusu-Ansah <ricki.owusu-ansah@georgebrown.ca>
2025-03-04 10:55:21 -0500

    basic file handling added

--- week9/sys_operations.py
+++ week9/sys_operations.py
@@ -36,3 +36,23 @@ print("Current Process ID:")
 print(os.getpid())
 
 print("--------------------\n")
+
+file_name = "fdpractice.txt"
+# Print the PID of the process before forking
+print(f"[Before Fork] Process ID: {os.getpid()}")
+
+#Open the file using os.open
+file_handle = os.open(file_name, os.O_RDWR | os.O_CREAT)
+print(f"\n[Process ID: {os.getpid()} opened file_handle: {file_handle}]")
+
+#Convert the file handle into a file object for writing
+file_object_TextIO = os.fdopen(file_handle, "w+")
+
+#Write text to the file
+file_object_TextIO.write("Lab Tues 10-12")
+file_object_TextIO.flush() # Ensure content is written immediately
+
+print(f"[Before Fork] Process ID: {os.getpid()}")
+
+
+
